e37744d0f93f149fe02d095902928a319f5e10ae
sajib689 <jibonraja11@gmail.com>
2025-03-19 20:08:19 +0600

    wrap application in Redux Provider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
+import store from './Redux/Store.js'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
